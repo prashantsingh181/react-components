c8a267c2772901f185409d34930d3aa0416ccc04
prashantsingh181 <prashant@poditservices.com>
2024-04-01 17:15:33 +0530

    scroll indicator completed

---
+++ src/components/scroll-indicator/index.jsx
@@ -0,0 +1,60 @@
+import "./styles.css";
+import React, { useEffect, useState } from "react";
+
+export default function ScrollIndicator({ url }) {
+    const [scrollIndicatorWidth, setScrollIndicatorWidth] = useState(0);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  async function fetchData(url) {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data.products);
+    } catch (error) {
+      console.error(error);
+      setErrorMessage(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
+
+  useEffect(() => {
+    function handleScroll(e){
+        const scrollY = window.scrollY;
+        const clientHeight = document.documentElement.clientHeight;
+        const scrollHeight = document.documentElement.scrollHeight;
+        setScrollIndicatorWidth((scrollY/(scrollHeight - clientHeight))*100)
+    }
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    }
+  }, []) 
+  if (loading) {
+    return <div>Loading</div>;
+  }
+  if (errorMessage) {
+    return <div>Error: {errorMessage}</div>;
+  }
+  return (
+    <div className="scroll-container">
+      <div className="scroll-header">
+        <h1>Scroll Indicator</h1>
+        <div className="scroll-indicator-container">
+          <div className="scroll-indicator" style={{width: `${scrollIndicatorWidth}%`}}></div>
+        </div>
+      </div>
+      <div className="data-container">
+        {data.map((item) => (
+          <p key={item.id}>{item.title}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
