01e7a452a18a025bc090cc9f44481b18c8e813c5
prashantsingh181 <prashant.singh181@gmail.com>
2024-07-14 21:40:20 +0530

    refactored star rating components

--- src/services/star-rating/index.jsx
+++ src/services/star-rating/index.jsx
@@ -1,32 +1,29 @@
-import "./styles.css";
 import { useState } from "react";
-export default function StarRating({ noOfStars = 5 }) {
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating({ numberOfStars = 5 }) {
   const [hoverIndex, setHoverIndex] = useState(-1);
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  function handleMouseEnter(index) {
-    setHoverIndex(index);
-  }
-  function handleMouseLeave() {
-    setHoverIndex(selectedIndex);
-  }
   function handleClick(index) {
     setSelectedIndex(index);
   }
 
+  function handleMouseEnter(index) {
+    setHoverIndex(index);
+  }
+
+  function handleMouseLeave() {
+    setHoverIndex(selectedIndex);
+  }
+
   return (
-    <div className="star-wrapper">
-      {[...Array(noOfStars)].map((_, i) => (
-        <div
-          key={i}
-          className={`star ${
-            i <= (hoverIndex ?? selectedIndex) ? "active" : "inactive"
-          }`}
-          onMouseEnter={() => handleMouseEnter(i)}
-          onMouseLeave={handleMouseLeave}
-          onClick={() => handleClick(i)}
-        ></div>
-      ))}
+    <div className="container mx-auto p-8 flex flex-col gap-8 items-center">
+      <h2 className="text-primary-text dark:text-primary-dark-text font-semibold text-2xl">Please give a rating</h2>
+      <div className="flex gap-3">
+        {Array(numberOfStars).fill().map((_, i) => <FaStar size="1.5rem" className={`cursor-pointer ${i <= hoverIndex ? "text-yellow-300" : "text-primary-dark-bg dark:text-primary-bg"}`} onClick={() => handleClick(i)} onMouseEnter={() => handleMouseEnter(i)} onMouseLeave={handleMouseLeave} />)}
+      </div>
+      {selectedIndex > -1 && <p>You gave a {selectedIndex + 1} star rating!</p>}
     </div>
-  );
+  )
 }
