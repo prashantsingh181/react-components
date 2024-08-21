73b0fa5db0c95193a29f99adc997d105c0c48331
prashantsingh181 <prashant.singh181@gmail.com>
2024-08-21 17:26:50 +0530

    routing changes for github pages

--- src/services/light-dark/ThemeContext.jsx
+++ src/services/light-dark/ThemeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useLayoutEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 const ThemeContext = createContext();
@@ -7,7 +7,7 @@ export const useTheme = () => useContext(ThemeContext);
 export default function ThemeContextProvider({ children }) {
   const [theme, setTheme] = useLocalStorage("theme", "light");
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
 
